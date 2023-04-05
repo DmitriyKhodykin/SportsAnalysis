928695c3bdd61fcdf60d7f69df854926ec351836
Dmitry.Khodykin <dmitry.hodykin@itentika.ru>
2023-04-05 10:58:11 +0300

    init

---
+++ ml_service/kick_recognition.py
@@ -0,0 +1,48 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret:
+            # Переводим картинку в серый цвет для лучшей работы алгоритма
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Используем алгоритм Canny для выделения контуров объектов на изображении
+            edges = cv2.Canny(gray, 100, 200)
+
+            # Определяем точки на теле футболиста с помощью Mediapipe
+            results = pose.process(frame)
+            landmarks = results.pose_landmarks
+
+            if landmarks is not None:
+                # Определяем координаты ключевых точек на теле футболиста
+                lankle = [int(landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].x * frame.shape[1]),
+                          int(landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y * frame.shape[0])]
+                rfoot = [int(landmarks.landmark[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX].x * frame.shape[1]),
+                         int(landmarks.landmark[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX].y * frame.shape[0])]
+
+                # Определяем координаты мяча на изображении
+                ball_cascade = cv2.CascadeClassifier('ball_cascade.xml')
+                balls = ball_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+                for (x, y, w, h) in balls:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Определяем, был ли сделан удар по мячу
+                if lankle[1] < rfoot[1] and (rfoot[0] - x) < 30 and (rfoot[1] - y) < 30:
+                    cv2.putText(frame, 'KICK!', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+
+            # Отображаем результат на экране
+            cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ ml_service/number_detection.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# cap = cv2.VideoCapture(0)  # Используем камеру
+cap = cv2.VideoCapture('video.mp4')  # Используем видеофайл
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
+        # Если видео, то 'break' вместо 'continue'
+        if not success:
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.pose_landmarks:
+            # Отображаем ключевые точки на изображении
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+            # Определяем номер футболиста
+            left_shoulder = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
+            right_shoulder = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+            left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
+            right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+
+            x1, y1 = int(left_shoulder.x * image.shape[1]), int(left_shoulder.y * image.shape[0])
+            x2, y2 = int(right_shoulder.x * image.shape[1]), int(right_shoulder.y * image.shape[0])
+            x3, y3 = int(left_hip.x * image.shape[1]), int(left_hip.y * image.shape[0])
+            x4, y4 = int(right_hip.x * image.shape[1]), int(right_hip.y * image.shape[0]) 
+            # Отслеживаем футболиста по номеру на футболке
+            # Например, если футболист с номером 10, то ищем прямоугольник в районе его номера
+            if 0.45 < left_shoulder.y < 0.55 and 0.45 < right_shoulder.y < 0.55 and \
+                    0.65 < left_hip.y < 0.75 and 0.65 < right_hip.y < 0.75:
+                roi = image[y1:y3, x1:x2]
+                hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                lower_red = np.array([0, 50, 50])
+            upper_red = np.array([10, 255, 255])
+            mask = cv2.inRange(hsv, lower_red, upper_red)
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                if w > 20 and h > 20:
+                    cv2.rectangle(image, (x1+x, y1+y), (x1+x+w, y1+y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Football player tracking', image)
